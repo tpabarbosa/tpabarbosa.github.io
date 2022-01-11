f2cf972022ade1a29b0d2df0683a7b84c7472a74
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-01-11 11:36:09 -0300

    build: add pkg jest-watch-typeahead@0.6.5
    Necessary to run tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
   render(<App />);
+});
+
+test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders image', () => {
+  const imageElement = screen.getByRole('img');
+  expect(imageElement).toHaveAttribute('src', 'logo.svg');
+  expect(imageElement).toHaveAttribute('alt', 'logo');
+})
