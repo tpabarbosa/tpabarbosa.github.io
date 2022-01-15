160554b897724c956023215060d9712e77f1bd6e
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-01-15 13:23:58 -0300

    fix: remove some unused imports

--- src/theme/ThemeTogglerButton/index.tsx
+++ src/theme/ThemeTogglerButton/index.tsx
@@ -1,16 +1,12 @@
-
-import React from 'react';
 import useTheme from '..';
-import * as S from './styles';
 
 export const ThemeTogglerButton = () => {
-    const [theme, themeToggler] = useTheme();
+  const [theme, themeToggler] = useTheme();
 
-    return (
-          <button onClick={themeToggler} className='icon-btn'>
-            {theme === 'light' ? <span >☀</span> : <span >🌙</span>}
-          </button>
-      );
+  return (
+        <button onClick={themeToggler} className='icon-btn'>
+          {theme === 'light' ? <span >☀</span> : <span >🌙</span>}
+        </button>
+    );
 }
-
 
