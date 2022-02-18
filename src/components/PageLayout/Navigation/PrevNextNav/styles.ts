636db288107b21ee0824dc6a0fdb943ddc484698
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-02-18 10:39:08 -0300

    refactor: add arrow buttons to mobile media

--- src/components/PageLayout/Navigation/PrevNextNav/styles.ts
+++ src/components/PageLayout/Navigation/PrevNextNav/styles.ts
@@ -1,37 +1,38 @@
 import styled from "styled-components";
 import media from "../../../../Theme/media";
 
-type ContainerProps = { 
-    type: 'prev' | 'next',
-}
+type ContainerProps = {
+  type: "prev" | "next";
+};
 
 export const Container = styled.nav<ContainerProps>`
-    width:1px;
-    height:1px;
-    overflow: hidden;
-    position: fixed;
-    top: 15vh;
-    ${props => props.type === 'prev' ? 'left: 0;' : 'right: 0;'};
-    z-index: 2;
-    
-    a { 
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        height: 100%;
-        
-        :hover {
-            color: ${props => props.theme.colorTwo};
+  //width: 10vw;
+  height: 50vh;
+  overflow: hidden;
+  position: fixed;
+  top: 15vh;
+  ${(props) => (props.type === "prev" ? "left: -30px;" : "right: -30px;")};
+  z-index: 2;
 
-            div {
-                background-color: ${props => props.theme.backgroundTwo};
-            }
-        }
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 100%;
+
+    :hover {
+      color: ${(props) => props.theme.colorTwo};
+
+      div {
+        background-color: ${(props) => props.theme.backgroundTwo};
+      }
     }
+  }
 
-    ${media.desktop`
-        width: 10vw;
-        height: 50vh
+  ${media.desktop`
+        ${(props: any) =>
+          props.type === "prev" ? "left: 0px;" : "right: 0px;"};
+        height: 50vh;
     `}
 `;
